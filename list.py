15ee85c77b4618ec172927dc9ebbb1471eb3cedd
monikagowda <109779401+monikagowda123@users.noreply.github.com>
2022-08-26 20:50:51 +0530

    Update list.py

--- list.py
+++ list.py
@@ -4,7 +4,7 @@ def quicksort(list, left, right):
         partition_pos=partition(list,left,right)
         quicksort(list, left, partition_pos-1)
         quicksort(list, partition_pos+1, right)
-
+hjutirwshs546
 
 list=[22, 11, 88, 66, 55, 77, 33, 44]
 quicksort=(list, 0, len(list) -1)
